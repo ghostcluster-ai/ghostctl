baa0680efbf6359e0329bb734f3d1bb10dc629a3
kamalvakkom <132481763+kamalvakkom@users.noreply.github.com>
2026-02-10 13:33:03 +0000

    Fix Homebrew formula tag specification

--- Formula/ghostctl.rb
+++ Formula/ghostctl.rb
@@ -2,8 +2,7 @@ class Ghostctl < Formula
   desc "CLI tool for managing ephemeral Kubernetes clusters using vCluster"
   homepage "https://github.com/ghostcluster-ai/ghostctl"
   url "https://github.com/ghostcluster-ai/ghostctl.git",
-      tag: "v1.0.0",
-      revision: "HEAD"
+      tag: "v1.0.0"
   version "1.0.0"
   license "Apache-2.0"
 
